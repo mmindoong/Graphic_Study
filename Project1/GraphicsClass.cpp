9376dfe4a223e85b7e6cb0217e6aa77dfaac8340
mmindoong <70145314+mmindoong@users.noreply.github.com>
2021-07-01 22:33:51 +0900

    버퍼,쉐이더 및 HLSL

--- Project1/GraphicsClass.cpp
+++ Project1/GraphicsClass.cpp
@@ -1,7 +1,9 @@
 #include "stdafx.h"
 #include "GraphicsClass.h"
 #include "D3DClass.h"
-
+#include "CameraClass.h"
+#include "ColorShaderClass.h"
+#include "ModelClass.h"
 
 GraphicsClass::GraphicsClass()
 {
@@ -20,26 +22,84 @@ GraphicsClass::~GraphicsClass()
 
 bool GraphicsClass::Initialize(int screenWidth, int screenHeight, HWND hwnd)
 {
-	//Direct3D 객체 생성
+	//D3DClass 객체 생성
 	m_Direct3D = (D3DClass*)_aligned_malloc(sizeof(D3DClass), 16);
 	if (!m_Direct3D)
 		return false;
 	
-	//Direct3D 객체 초기화
+	// D3DClass 객체 초기화
 	if (!m_Direct3D->Initialize(screenWidth, screenHeight, VSYNC_ENABLED, hwnd,
 		FULL_SCREEN, SCREEN_DEPTH, SCREEN_NEAR))
 	{
 		MessageBox(hwnd, L"Could not initialize Direct3D", L"Error", MB_OK);
 		return false;
 	}
-	return true;
 
+	//CameraClass객체 생성
+	m_Camera = new CameraClass;
+	if (!m_Camera)
+	{
+		return false;
+	}
+	//카메라 포지션 설정
+	m_Camera->SetPosition(0.0f, 0.0f, -5.0f);
+
+	//ModelClass객체 생성
+	m_Model = new ModelClass;
+	if (!m_Model)
+	{
+		return false;
+	}
+	//ModelClass객체 초기화
+	if (!m_Model->Initialize(m_Direct3D->GetDevice()))
+	{
+		MessageBox(hwnd, L"Could not initialize the model object", L"Error", MB_OK);
+		return false;
+	}
+
+	//ColorShaderClass객체 생성
+	m_ColorShader = new ColorShaderClass;
+	if (!m_ColorShader)
+	{
+		return false;
+	}
+	//ColorShaderClass객체 초기화
+	if (!m_ColorShader->Initialize(m_Direct3D->GetDevice(), hwnd))
+	{
+		MessageBox(hwnd, L"Could not initialize the color shader object", L"Error", MB_OK);
+		return false;
+	}
+
+	return true;
 }
 
 
 void GraphicsClass::Shutdown()
 {
-	//Direct3d 객체 반환
+	//m_ColorShader객체 반환
+	if (m_ColorShader)
+	{
+		m_ColorShader->Shutdown();
+		delete m_ColorShader;
+		m_ColorShader = 0;
+	}
+
+	//m_Model객체 반환
+	if (m_Model)
+	{
+		m_Model->Shutdown();
+		delete m_Model;
+		m_Model = 0;
+	}
+
+	//m_Camera 객체 반환
+	if (m_Camera)
+	{
+		delete m_Camera;
+		m_Camera = 0;
+	}
+
+	//D3DClass객체 반환
 	if (m_Direct3D)
 	{
 		m_Direct3D->Shutdown();
@@ -58,10 +118,29 @@ bool GraphicsClass::Frame()
 
 bool GraphicsClass::Render()
 {
-	//씬을 그리기 위해 버퍼를 지움.
-	m_Direct3D->BeginScene(0.5f, 0.5f, 0.5f, 1.0f);
+	// 씬을 그리기 위해 버퍼를 지웁니다
+	m_Direct3D->BeginScene(0.0f, 0.0f, 0.0f, 1.0f);
 
-	//버퍼의 내용을 화면에 출력
+	// 카메라의 위치에 따라 뷰 행렬을 생성합니다
+	m_Camera->Render();
+
+	// 카메라 및 d3d 객체에서 월드, 뷰 및 투영 행렬을 가져옵니다
+	XMMATRIX worldMatrix, viewMatrix, projectionMatrix;
+	m_Direct3D->GetWorldMatrix(worldMatrix);
+	m_Camera->GetViewMatrix(viewMatrix);
+	m_Direct3D->GetProjectionMatrix(projectionMatrix);
+
+	// 모델 버텍스와 인덱스 버퍼를 그래픽 파이프 라인에 배치하여 드로잉을 준비합니다.
+	m_Model->Render(m_Direct3D->GetDeviceContext());
+
+	// 색상 쉐이더를 사용하여 모델을 렌더링합니다.
+	if (!m_ColorShader->Render(m_Direct3D->GetDeviceContext(), m_Model->GetIndexCount(), worldMatrix, viewMatrix, projectionMatrix))
+	{
+		return false;
+	}
+
+	// 버퍼의 내용을 화면에 출력합니다
 	m_Direct3D->EndScene();
+
 	return true;
 }
--- Project1/GraphicsClass.h
+++ Project1/GraphicsClass.h
@@ -9,6 +9,10 @@ const float SCREEN_DEPTH = 1000.0f;
 const float SCREEN_NEAR = 0.1f;
 
 class D3DClass;
+class CameraClass;
+class ModelClass;
+class ColorShaderClass;
+
 class GraphicsClass
 {
 public:
@@ -23,4 +27,7 @@ public:
 private:
 	bool Render();
 	D3DClass* m_Direct3D = nullptr;
+	CameraClass* m_Camera = nullptr;
+	ModelClass* m_Model = nullptr;
+	ColorShaderClass* m_ColorShader = nullptr;
 };
