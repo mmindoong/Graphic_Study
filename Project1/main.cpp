994a9a7cd901aa663ce68cf0e3a995f390f15ec3
mmindoong <70145314+mmindoong@users.noreply.github.com>
2021-07-01 19:03:27 +0900

    초기 커밋.

---
+++ Project1/main.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "SystemClass.h"
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
+{
+	SystemClass* System = new SystemClass;
+	if (!System)
+	{
+		return -1;
+	}
+
+	if (System->Initialize())
+	{
+		System->Run();
+	}
+
+	System->Shutdown();
+	delete System;
+	System = nullptr;
+
+	return 0;
+}
